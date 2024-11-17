91739a74d9a8be5dc957fdd4a0610d1e990ba3c9
lee71824918 <lee71824918@gmail.com>
2024-11-18 02:29:35 +0900

    v4.1

--- src/pages/config.js
+++ src/pages/config.js
@@ -1,4 +1,5 @@
-export const apiUrl = "http://localhost:4001"
+// export const apiUrl = "http://localhost:4001"
+export const apiUrl = "https://intermediate-lenore-asdasd30-1a679ee0.koyeb.app"
 
 //백엔드 주소
 
